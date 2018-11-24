fa8f8da600b499a8d743b9fef9037ad19de56502
Darren <491361994@qq.com>
2018-11-24 17:19:39 +0800

    first commit

---
+++ precache-manifest.81a163b0c4209810557a8e74d3667c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5c25040bc4cc4d65e25",
+    "url": "/reactfinance/static/js/runtime~main.d5c25040.js"
+  },
+  {
+    "revision": "c94b42d85e903e1f15ca",
+    "url": "/reactfinance/static/js/main.c94b42d8.chunk.js"
+  },
+  {
+    "revision": "ce2fefb01b0a66436a21",
+    "url": "/reactfinance/static/js/1.ce2fefb0.chunk.js"
+  },
+  {
+    "revision": "c94b42d85e903e1f15ca",
+    "url": "/reactfinance/static/css/main.33a54e88.chunk.css"
+  },
+  {
+    "revision": "a881a13bc8772fc03394bccd928840e4",
+    "url": "/reactfinance/index.html"
+  }
+];
